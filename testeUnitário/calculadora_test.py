822a5cc1314cf358b7a35dbc258d643fc182e18e
Lucca Sindeaux <luccasindeaux@gmail.com>
2025-04-10 20:29:41 -0300

    testes unitários

---
+++ testeUnitário/calculadora_test.py
@@ -0,0 +1,14 @@
+import unittest
+from calculadora import somar, dividir
+
+class TestCalculadora(unittest.TestCase):
+
+    def test_somar(self):
+        self.assertEqual(somar(2, 3), 5)
+        self.assertEqual(somar(-1, 1), 0)
+        self.assertEqual(somar(0, 0), 0)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(8,4), 2)
+        self.assertEqual(dividir(6,3), 2)
+        self.assertEqual(dividir(7,2), 3.5)
